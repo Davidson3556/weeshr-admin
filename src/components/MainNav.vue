91b46bcb50a8ffa10d7c990b994771cdf5fe514d
sankore <eugene@sankore.com>
2024-01-13 16:24:19 +0100

    conflict resolved

---
+++ src/components/MainNav.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+</script>
+
+<template>
+  <nav
+    :class="cn('flex items-center space-x-4 lg:space-x-6', $attrs.class ?? '')"
+  >
+    <a
+      class="text-sm font-medium transition-colors hover:text-primary cursor-pointer"
+    >
+      Overview
+    </a>
+    <a
+      aria-disabled="true"
+      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
+    >
+      Customers
+    </a>
+    <a
+      aria-disabled="true"
+      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
+    >
+      Products
+    </a>
+
+    <a
+      aria-disabled="true"
+      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
+    >
+      Settings
+    </a>
+  </nav>
+</template>
