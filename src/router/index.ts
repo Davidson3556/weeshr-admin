9f3df54800c1371ca0bf86d4420db69611074718
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-01-22 14:40:20 +0100

    src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import Errorpage404 from '../views/Errorpage404.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/errorpage',
+    name: 'error',
+    component: Errorpage404,
+  },
+  // Added the wildcard route for handling 404 errors here
+  {
+    path: '/:catchAll(.*)',
+    name: 'not-found',
+    component: Errorpage404,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
+  routes,
+});
 
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue')
-    },
-
-    {
-      path: '/errorpage',
-      name: 'error',
-      component: () => import('../views/Errorpage404.vue')
-    }
-
-  ]
-})
-
-export default router
+export default router;
