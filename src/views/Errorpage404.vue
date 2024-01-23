74abe3c0a165017b030bb3c67bc0ca5771e0dbdc
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-01-23 16:53:25 +0100

    modified:   src/views/Errorpage404.vue

--- src/views/Errorpage404.vue
+++ src/views/Errorpage404.vue
@@ -13,15 +13,15 @@
 <div class="h-screen w-screen bg-gray-50 flex items-center">
     <div class="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
             <div class="w-full lg:w-1/2 mx-8">
-                <div class="text-7xl text-green-500 font-dark font-extrabold mb-8">  404 NOT FOUND </div>
+                <div class="text-7xl text-blue-500 font-dark font-extrabold mb-8">  404 - ERROR </div>
             <p class="text-2xl md:text-3xl font-light leading-normal mb-8">
                 Oops! The page you are looking for could not be found.
             </p>
             
-            <a href="/" class="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-blue-600 hover:bg-blue-700">HOMEPAGE</a>
+            <a href="/" class="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-blue-600 active:bg-blue-600 hover:bg-green-700">HOMEPAGE</a>
     </div>
         <div class="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
-        <img src="https://user-images.githubusercontent.com/43953425/166269493-acd08ccb-4df3-4474-95c7-ad1034d3c070.svg" class="" alt="Page not found">
+        <img src="https://res.cloudinary.com/dufimctfc/image/upload/v1706025065/Presentation1_q2qrbv.jpg" class="" alt="Page not found">
         </div>
     
     </div>
