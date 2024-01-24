85b7148ff40372b42d6ab1df520583b71c9a605d
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-01-24 12:15:32 +0100

    route for handling 404 errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import LoginView from '../views/LoginView.vue';
-import Errorpage404 from '../views/Errorpage404.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import Errorpage404 from '../views/Errorpage404.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: HomeView
   },
   {
     path: '/login',
     name: 'login',
-    component: LoginView,
+    component: LoginView
   },
   {
     path: '/errorpage',
     name: 'error',
-    component: Errorpage404,
+    component: Errorpage404
   },
   // Added the wildcard route for handling 404 errors here
   {
     path: '/:catchAll(.*)',
     name: 'not-found',
-    component: Errorpage404,
-  },
-];
+    component: Errorpage404
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
--- src/views/Errorpage404.vue
+++ src/views/Errorpage404.vue
@@ -1,30 +1,31 @@
-<script setup lang="ts">
-
-
-
-
-
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <section>
+    <div class="h-screen w-screen bg-gray-50 flex items-center">
+      <div
+        class="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700"
+      >
+        <div class="w-full lg:w-1/2 mx-8">
+          <div class="text-7xl text-blue-500 font-dark font-extrabold mb-8">404 - ERROR</div>
+          <p class="text-2xl md:text-3xl font-light leading-normal mb-8">
+            Oops! The page you are looking for could not be found.
+          </p>
 
-<div class="h-screen w-screen bg-gray-50 flex items-center">
-    <div class="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
-            <div class="w-full lg:w-1/2 mx-8">
-                <div class="text-7xl text-blue-500 font-dark font-extrabold mb-8">  404 - ERROR </div>
-            <p class="text-2xl md:text-3xl font-light leading-normal mb-8">
-                Oops! The page you are looking for could not be found.
-            </p>
-            
-            <a href="/" class="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-blue-600 active:bg-blue-600 hover:bg-green-700">HOMEPAGE</a>
-    </div>
-        <div class="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
-        <img src="https://res.cloudinary.com/dufimctfc/image/upload/v1706025065/Presentation1_q2qrbv.jpg" class="" alt="Page not found">
+          <a
+            href="/"
+            class="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-blue-600 active:bg-blue-600 hover:bg-green-700"
+            >HOMEPAGE</a
+          >
         </div>
-    
+        <div class="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
+          <img
+            src="https://res.cloudinary.com/dufimctfc/image/upload/v1706025065/Presentation1_q2qrbv.jpg"
+            class=""
+            alt="Page not found"
+          />
+        </div>
+      </div>
     </div>
-</div>
-</section>
+  </section>
 </template>
