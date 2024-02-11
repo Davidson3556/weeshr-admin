bf86b7923d2db27343e0044028a217bb719d43a9
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-02-11 19:24:03 +0100

    modified:   src/views/Permissionpage.vue

--- src/views/Permissionpage.vue
+++ src/views/Permissionpage.vue
@@ -1,88 +1,166 @@
+<script setup lang="ts">
+import Overview from '@/components/UseOverview.vue'
+import DateRangePicker from '@/components/DateRangePicker.vue'
+import MainNav from '@/components/MainNav.vue'
+import Search from '@/components/UseSearch.vue'
+import UserNav from '@/components/UserNav.vue'
+
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+</script>
 
 <template>
-  <div class="container mx-auto p-4">
-    <div class="flex items-center justify-between mb-4">
-      <h2 class="text-2xl font-semibold">Permission Page</h2>
-      <router-link to="/" class="text-blue-500">Back to Home</router-link>
-    </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <!-- Role Selection -->
-      <div>
-        <h3 class="text-lg font-semibold mb-2">Select Role</h3>
-        <select v-model="selectedRole" class="w-full p-2 border rounded">
-          <option v-for="role in roles" :key="role.id" :value="role.id">{{ role.roleName }}</option>
-        </select>
+
+  <div class=" flex-col flex h-svh h-full bg-[#f0f8ff]">
+    
+    <div class="flex-1 space-y-4 p-8 pt-6 bg-[#f0f8ff]  ">
+      <div class="flex items-center justify-between space-y-2 flex-col sm:flex-row">
+        <h2 class="text-3xl font-bold tracking-tight text-blue-900">
+          Permission page
+        </h2>
+        <div class="flex items-center space-x-2">
+          <Button>Back to Home</Button>
+        </div>
       </div>
+      <Tabs default-value="overview" class="space-y-4">
+        <TabsList class="text-blue-900">
+          <TabsTrigger value="overview" class="text-blue-900 active:text-blue-900">
+            Read
+          </TabsTrigger>
+          <TabsTrigger value="analytics" disabled>
+            Create
+          </TabsTrigger>
+          <TabsTrigger value="reports" disabled>
+            Update
+          </TabsTrigger>
+          <TabsTrigger value="notifications" disabled>
+            Delete
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="overview" class="space-y-4">
+         
+          <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+            <Card class="col-span-4">
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Employees</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
 
-      <!-- Permissions Table -->
-      <div class="col-span-2">
-        <h3 class="text-lg font-semibold mb-2">Permissions</h3>
-        <table class="w-full border border-collapse">
-          <thead>
-            <tr class="bg-gray-200">
-              <th class="p-2">Resource Name</th>
-              <th class="p-2">Read</th>
-              <th class="p-2">Create</th>
-              <th class="p-2">Update</th>
-              <th class="p-2">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(resource, index) in resources" :key="index">
-              <td class="p-2">{{ resource.resourceName }}</td>
-              <td class="p-2"><input type="checkbox" v-model="resource.read" /></td>
-              <td class="p-2"><input type="checkbox" v-model="resource.create" /></td>
-              <td class="p-2"><input type="checkbox" v-model="resource.update" /></td>
-              <td class="p-2"><input type="checkbox" v-model="resource.delete" /></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+                </CardDescription>
+              </CardHeader>
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Subscription History</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.  You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Warehouse Zone</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.  You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Warehouse Bin Location</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.  You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Inventory Items</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.  You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+            <Card class=" flex w-full col-span-full grid sm:col-span-3">
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Roles</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.   You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Warehouse</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.  You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
+              
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Warehouse Shelves</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.  You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
 
-    <!-- Assign Button -->
-    <div class="mt-4">
-      <button @click="assignPermissions" class="bg-blue-500 text-white px-4 py-2 rounded">
-        Assign Permissions
-      </button>
+              <CardHeader>
+                <CardTitle><input type="checkbox" id="checkbox5" name="checkbox5" />
+            <label for="checkbox5">View Item Categories</label></CardTitle>
+                
+                <CardDescription>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.  You made 265 sales this month.
+                </CardDescription>
+              </CardHeader>
+
+              <CardContent>
+              </CardContent>
+            </Card>
+            
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   </div>
 </template>
-
-
-  
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  data() {
-    return {
-      roles: [
-        { id: 1, roleName: 'Super Admin' },
-        { id: 2, roleName: 'Admin' },
-        { id: 3, roleName: 'Editor' },
-      ],
-      selectedRole: 1,
-      resources: [
-        { resourceName: 'View Employees', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Subscription History', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Warehouse Zone', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Warehouse Bin Location', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Inventory Items', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Roles', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Warehouse', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Warehouse Shelves', read: false, create: false, update: false, delete: false },
-        { resourceName: 'View Item Categories', read: false, create: false, update: false, delete: false },
-      ],
-    };
-  },
-  methods: {
-    assignPermissions(): void {
-      // Implement logic to assign permissions here
-      console.log('Permissions assigned!');
-    },
-  },
-});
-</script>
 
