f7a1c4f43031fac158585f1a899de03e56e55e3c
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-02-14 23:21:38 +0100

    modified:   src/components/MainNav.vue

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,44 +3,19 @@ import { cn } from '@/lib/utils'
 </script>
 
 <template>
-  <nav
-    :class="cn('flex items-center space-x-4 lg:space-x-6', $attrs.class ?? '')"
-  >
+  <nav :class="cn('flex items-center space-x-4 lg:space-x-6', $attrs.class ?? '')">
+    <a class="text-sm font-medium transition-colors hover:text-primary cursor-pointer" href="/">Overview</a>
     
-    <a
-      class="text-sm font-medium transition-colors hover:text-primary cursor-pointer"
-    >
-    <a href="/">Overview</a>
-  </a> 
-    
-    <a
-      aria-disabled="true"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
-    >
+    <a aria-disabled="true" class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer">
       Customers
     </a>
     
-    <a
-      aria-disabled="true"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
-    >
+    <a aria-disabled="true" class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer">
       Products
     </a>
 
-    <!-- <a
-      aria-disabled="true"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
-    >
-      Settings
-    </a> -->
-
-     <a
-      aria-disabled="true"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
-    >
-    <a href="/permissionpage">Permission</a>
-    </a> 
-       
-    
+    <a aria-disabled="true" class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer" href="/permissionpage">
+      Permission
+    </a>
   </nav>
 </template>
