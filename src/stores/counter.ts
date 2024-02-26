d27314fdf3231b7abc4f32ca1625ce3dfbe7b26f
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-02-26 22:57:08 +0100

    modified:   src/stores/counter.ts

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,25 +1,12 @@
 import { ref, computed } from 'vue'
-import { defineStore } from 'pinia';
-import { usePermissionStore } from './permission'; 
+import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
-  const permissionStore = usePermissionStore();
-
-  const count = ref(0);
-  const doubleCount = computed(() => count.value * 2);
-
+  const count = ref(0)
+  const doubleCount = computed(() => count.value * 2)
   function increment() {
-    count.value++;
+    count.value++
   }
 
-  // Access permission store properties and methods
-  function setAdmin(value: boolean) {
-    permissionStore.setAdmin(value);
-  }
-
-  function setCanEdit(value: boolean) {
-    permissionStore.setCanEdit(value);
-  }
-
-  return { count, doubleCount, increment, setAdmin, setCanEdit };
-});
+  return { count, doubleCount, increment }
+})
