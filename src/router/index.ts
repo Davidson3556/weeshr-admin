37f802759c2ed8b72bcace104dfe7204f57f5135
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-02-26 22:09:50 +0100

    ERRORPAGE.VUE

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import Errorpage404 from '../views/Errorpage.vue'
-import Permissionpage from '../views/Permissionpage.vue'
 
 const routes = [
   {
@@ -15,26 +14,19 @@ const routes = [
     name: 'login',
     component: LoginView
   },
-  {
-    path: '/permissionpage',
-    name: 'permission',
-    component: Permissionpage
-  },
+  
   {
     path: '/errorpage',
     name: 'error',
     component: Errorpage404
   },
-
-  
+ 
   // Added the wildcard route for handling 404 errors here
   {
     path: '/:catchAll(.*)',
     name: 'not-found',
     component: Errorpage404
   }
-
- 
 ]
 
 const router = createRouter({
--- src/views/Errorpage.vue
+++ src/views/Errorpage.vue
@@ -1,31 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-vue-next'
 
+</script>
 <template>
-  <section>
-    <div class="h-screen w-screen bg-gray-50 flex items-center">
-      <div
-        class="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700"
-      >
-        <div class="w-full lg:w-1/2 mx-8">
-          <div class="text-7xl text-blue-500 font-dark font-extrabold mb-8">404 - ERROR</div>
-          <p class="text-2xl md:text-3xl font-light leading-normal mb-8">
-            Oops! The page you are looking for could not be found.
-          </p>
+  <div class="max-w-full text-center">
+    <img
+      class="w-full h-auto"
+      src="https://res.cloudinary.com/dufimctfc/image/upload/v1708972628/Container_rydkpd.svg"
+      alt=""
+    />
+    <div class="flex justify-center items-center mt-4">
 
-          <a
-            href="/"
-            class="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-blue-600 active:bg-blue-600 hover:bg-green-700"
-            >HOMEPAGE</a
-          >
-        </div>
-        <div class="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
-          <img
-            src="https://res.cloudinary.com/dufimctfc/image/upload/v1706025065/Presentation1_q2qrbv.jpg"
-            class=""
-            alt="Page not found"
-          />
-        </div>
-      </div>
+     <Button class="w-auto  sm:w-auto bg-lime-500 hover:bg-lime-600 ">
+        <a href="/login" class="block p-4 text-black">
+          Sign in
+        </a>
+      </Button>
+
     </div>
-  </section>
+  </div>
 </template>
