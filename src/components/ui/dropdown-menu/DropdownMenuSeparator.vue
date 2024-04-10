7b8f83454e801d127321226b10ba1de1a75448bf
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-04-10 17:03:08 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/components/ui/dropdown-menu/DropdownMenu.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuCheckboxItem.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuContent.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuItem.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuLabel.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuRadioGroup.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuRadioItem.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuSeparator.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuSub.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuSubContent.vue
    	modified:   src/components/ui/dropdown-menu/DropdownMenuSubTrigger.vue

--- src/components/ui/dropdown-menu/DropdownMenuSeparator.vue
+++ src/components/ui/dropdown-menu/DropdownMenuSeparator.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { type HTMLAttributes, computed } from 'vue'
-import { DropdownMenuSeparator, type DropdownMenuSeparatorProps } from 'radix-vue'
+import {
+  DropdownMenuSeparator,
+  type DropdownMenuSeparatorProps,
+} from 'radix-vue'
 import { cn } from '@/lib/utils'
 
-const props = defineProps<
-  DropdownMenuSeparatorProps & {
-    class?: HTMLAttributes['class']
-  }
->()
+const props = defineProps<DropdownMenuSeparatorProps & {
+  class?: HTMLAttributes['class']
+}>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
@@ -17,8 +18,5 @@ const delegatedProps = computed(() => {
 </script>
 
 <template>
-  <DropdownMenuSeparator
-    v-bind="delegatedProps"
-    :class="cn('-mx-1 my-1 h-px bg-muted', props.class)"
-  />
+  <DropdownMenuSeparator v-bind="delegatedProps" :class="cn('-mx-1 my-1 h-px bg-muted', props.class)" />
 </template>
